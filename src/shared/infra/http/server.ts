804321f4208d06cff13cdaa5af7c9e80483136f2
tomazcx <tomazcx06@gmail.com>
2023-04-25 12:53:50 -0300

    fixed: changed port variable name

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,3 @@
 import app from "./app";
 
-app.listen(process.env.port || 3000, () => console.log(`Server iniciado na porta ${process.env.port || 3000}`))
+app.listen(process.env.PORT || 3000, () => console.log(`Server iniciado na porta ${process.env.PORT || 3000}`))
